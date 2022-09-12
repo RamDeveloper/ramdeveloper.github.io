bc3a36b345c068319f443421b140bc5929fd7f97
ramkumarfs <sramkumar@farshore.com>
2022-09-12 16:45:23 +0530

    reverted the old site for temporary

--- src/data/contactsData.js
+++
@@ -1,6 +0,0 @@
-export const contactsData = {
-  email: "ramkumarpsy(at)gmail(dot)com",
-  phone: "9 5 6 6 7 - 3 6 3 7 5",
-  address: "Madurai, Tamilnadu",
-  sheetAPI: "https://sheetdb.io/api/v1/p74g4fsqax1h4",
-};
